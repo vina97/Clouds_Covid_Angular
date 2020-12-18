682378f9fd4e9ed445dfc109377c167add31f84e
vina97 <vinai@hotmail.it>
2020-12-18 17:40:01 +0100

    Add country info retrival, things to be refactored and fixed

--- src/app/country/country.component.ts
+++ src/app/country/country.component.ts
@@ -3,6 +3,8 @@ import { ActivatedRoute } from '@angular/router';
 import { Info } from '../info.model';
 import { ChartType, ChartOptions, ChartDataSets } from 'chart.js';
 import { Color, Label } from 'ng2-charts';
+import { CovidService } from '../covid.service';
+import { formatDate } from '@angular/common';
 
 @Component({
   selector: 'app-country',
@@ -62,16 +64,87 @@ export class CountryComponent implements OnInit {
 
 
   public total: Info = new Info("test", 0, 0, 0, 0, 0, 0)
-  public byCountry: Info[];
   public lastSeven: Array<Info>;
   public fromApril: Array<Info>;
 
-  constructor(private route: ActivatedRoute) { }
+
+
+  constructor(private route: ActivatedRoute, public covidService: CovidService) { }
+
+
 
   ngOnInit(): void {
     this.country = this.route.snapshot.paramMap.get('name')
-    console.log(this.country)
-    //Do update of DB
+    this.covidService.getCountryInfoDB(this.country).subscribe((res) => {
+      this.total = new Info(res.data()["name"], res.data()["totalCases"], res.data()["newCases"], res.data()["totalRecovery"], res.data()["newRecovery"], res.data()["totalDeath"], res.data()["newDeath"])
+      this.pieChartData = [this.total.totalDeath, this.total.totalRecovery, this.total.totalCases]
+    })
+
+    this.covidService.getLastSevenFromCountry(this.country).subscribe((data) => {
+      console.log(data)
+      let sortable = []
+      for (let elem in data) {
+        sortable.push(data[elem])
+      }
+      sortable.sort(function (a, b) {
+        var x = a["Confirmed"]; var y = b["Confirmed"];
+        return ((x < y) ? -1 : ((x > y) ? 1 : 0));
+      })
+      let newConf = []
+      let newDeath = []
+      let newRec = []
+      let i = 7
+      let labels = []
+      for (let elem in sortable) {
+        newConf.push(sortable[elem]["Confirmed"])
+        newRec.push(sortable[elem]["Recovered"])
+        newDeath.push(sortable[elem]["Deaths"])
+        labels.push(formatDate(new Date(new Date().getTime() - (i * 24 * 60 * 60 * 1000)), 'dd MMM', 'en'))
+        i = i - 1
+      }
+      //newConf.push(this.total["NewConfirmed"])
+      //newRec.push(this.total["NewRecovered"])
+      //newDeath.push(this.total["NewDeath"])
+      labels.push(formatDate(new Date(new Date().getTime()), 'dd MMM', 'en'))
+      this.barChartLabels = labels
+      this.barChartData = [{ data: newDeath, label: "New Death" }, { data: newRec, label: "New Recovered" }, { data: newConf, label: "New Confirmed" }]
+    })
+
+    this.covidService.getAllFromCountry(this.country).subscribe(data => {
+      let sortable = []
+      for (let elem in data) {
+        sortable.push(data[elem])
+      }
+
+      let totalConf = []
+      let totalDeath = []
+      let totalRec = []
+      let i = sortable.length
+      let labels = []
+      for (let elem in sortable) {
+        totalConf.push(sortable[elem]["Confirmed"])
+        totalRec.push(sortable[elem]["Recovered"])
+        totalDeath.push(sortable[elem]["Deaths"])
+        labels.push(formatDate(new Date(new Date().getTime() - (i * 24 * 60 * 60 * 1000)), 'dd MMM', 'en'))
+        i = i - 1
+      }
+      this.lineChartLabels = labels;
+      totalConf.sort(function (a, b) {
+        var x = a; var y = b;
+        return ((x < y) ? -1 : ((x > y) ? 1 : 0));
+      })
+      totalDeath.sort(function (a, b) {
+        var x = a; var y = b;
+        return ((x < y) ? -1 : ((x > y) ? 1 : 0));
+      })
+      totalRec.sort(function (a, b) {
+        var x = a; var y = b;
+        return ((x < y) ? -1 : ((x > y) ? 1 : 0));
+      })
+      this.lineChartData = [{ data: totalDeath, label: "Total Deaths" }, { data: totalRec, label: "Total Recoveries" }, { data: totalConf, label: "Total Confirmed" }]
+    }
+
+    )
   }
 
   numberWithCommas(x) {
--- src/app/covid.service.ts
+++ src/app/covid.service.ts
@@ -1,6 +1,12 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { formatDate } from '@angular/common';
+import firebase from "firebase/app";
+import { AngularFireAuth } from '@angular/fire/auth'
+import { AngularFirestore } from '@angular/fire/firestore'
+import { Router } from '@angular/router';
+import { Info } from './info.model';
+
 
 
 @Injectable({
@@ -9,7 +15,8 @@ import { formatDate } from '@angular/common';
 export class CovidService {
 
   private covid_API = 'https://api.covid19api.com/';  // URL to web api
-  constructor(private http: HttpClient,) { }
+  constructor(private http: HttpClient, private afAuth: AngularFireAuth,
+    private router: Router, private firestore: AngularFirestore) { }
 
   getSummary() {
     let options: { responseType?: "json" }
@@ -29,5 +36,66 @@ export class CovidService {
     let from = "2020-04-13"
     let options: { responseType?: "json" }
     return this.http.get(this.covid_API + "world?from=" + from + "&to=" + now, options)
+  }
+
+  public updateCountryInfo(country: Info) {
+    this.firestore.collection("Countries").doc(country.name).set({
+      name: country.name,
+      totalCases: country.totalCases,
+      newCases: country.newCases,
+      totalRecovery: country.totalRecovery,
+      newRecovery: country.newRecovery,
+      totalDeath: country.totalDeath,
+      newDeath: country.newDeath,
+      activeCases: country.activeCases,
+      recoveryRate: country.recoveryRate,
+      mortalityRate: country.mortalityRate,
+      lastUpdate: new Date()
+    }, { merge: true })
+  }
+
+  getAllFromCountry(country_name: string) {
+    let options: { responseType?: "json" }
+    return this.http.get(this.covid_API + "dayone/country/" + country_name, options)
+  }
+
+  getLastSevenFromCountry(country_name: string) {
+    let now = formatDate(new Date(), 'yyyy-MM-dd', 'en');
+    let from = formatDate(new Date(new Date().getTime() - (7 * 24 * 60 * 60 * 1000)), 'yyyy-MM-dd', 'en');
+    let options: { responseType?: "json" }
+    console.log(this.covid_API + "country/" + country_name + "?from=" + from + "&to=" + now, options)
+    return this.http.get(this.covid_API + "country/" + country_name + "?from=" + from + "&to=" + now, options)
+
+  }
+
+  getCountryInfoAPI(country_name: string) {
+    this.getSummary().toPromise().then(data => {
+      let elem = data["Countries"][0]
+      this.updateCountryInfo(new Info(elem["Country"], elem["TotalConfirmed"], elem["NewConfirmed"], elem["TotalRecovered"], elem["NewRecovered"], elem["TotalDeaths"], elem["NewDeaths"]))
+    })
+  }
+
+
+  public getCountryInfoDB(country_name: string) {
+
+    this.firestore.collection("Countries").doc(country_name).get().subscribe((doc) => {
+      let last: Date
+
+      let now = new Date()
+      if (doc.exists) {
+        last = doc.data()["lastUpdate"].toDate()
+        console.log(last)
+
+        if (last.getFullYear() === now.getFullYear() &&
+          last.getMonth() === now.getMonth() &&
+          last.getDate() === now.getDate()) {
+          console.log("TRUE")
+          return doc.data()
+        }
+      }
+
+      this.getCountryInfoAPI(country_name)
+    })
+    return this.firestore.collection("Countries").doc(country_name).get()
   }
 }
