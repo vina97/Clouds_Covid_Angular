a24f4d0863a75d5b3e2bee8cd61086fbefe712e0
vina97 <vinai@hotmail.it>
2021-01-16 17:43:31 +0100

    specific news route added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { CountryComponent } from './country/country.component';
 import { HomepageComponent } from './homepage/homepage.component';
 import { NewsComponent } from './news/news.component';
+import { NewspageComponent } from './newspage/newspage.component';
 
 const routes: Routes = [
   { path: "home", component: HomepageComponent },
   { path: "news", component: NewsComponent },
   { path: "country/:name", component: CountryComponent },
+  { path: "news/:id", component: NewspageComponent },
   { path: "", pathMatch: "full", redirectTo: "home" },
   { path: "**", redirectTo: "home" }
 ];
